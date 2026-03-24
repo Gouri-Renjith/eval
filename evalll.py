1bdd71d171447d8e77b480ca436971ce6c6f765d
ANORA\gouri.renjith <gouri.renjith@anorasolutions.com>
2026-03-24 15:17:16 +0530

    evals

---
+++ evalll.py
@@ -0,0 +1,20 @@
+import json
+
+filename="eval2.json"
+
+with open(filename,"r") as f:
+    data=json.load(f)
+
+data.reverse()
+
+new_data=[]
+
+for entry in data:
+    entry=dict(reversed(list(entry.items())))
+
+for item in data:
+    item={v:k for k,v in item.items()}
+    new_data.append(item)
+
+with open(filename,"w") as f:
+    json.dump(new_data,f,indent=4)
